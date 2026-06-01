ed7cce90eed4e5c5b9911ece605750d5f0bd027b
Nathaniel Poppe <poppent@mail.uc.edu>
2026-06-01 15:11:14 -0400

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[][] Merge(int[][] intervals) {
+        List<int[]> result = new List<int[]>();
+        intervals.Sort(x => x[0]);
+        foreach (var interval in intervals)
+        {
+            Console.WriteLine("${interval}")
+        }
+    }
+}
