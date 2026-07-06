ca5534fa4926e13a7de355bd4fcd2dec5549eb77
Nathaniel Poppe <poppent@mail.uc.edu>
2026-07-06 19:45:31 -0400

    Add: non-overlapping-intervals - submission-3

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        def is_overlapping(i1, i2):
+            print(f"checking overlap for {i1=} and {i2=}")
+            return i1[0] < i2[1] and i2[0] < i1[1]
+
+        intervals.sort(key=lambda x: x[0])
+        print(f"pre {intervals=}")
+
+        i = 0
+        res = 0
+        while i < len(intervals) - 1:
+            print(f"iter {i=} {intervals=}")
+            if is_overlapping(intervals[i], intervals[i + 1]):
+                print("overlapping")
+                intervals.pop(i + 1)
+                res += 1
+            else:
+                i += 1
+
+        return res
