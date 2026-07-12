0a766ccdf2d5128f264f12c8b7868006b1a92e0f
Nathaniel Poppe <poppent@mail.uc.edu>
2026-07-12 19:53:56 -0400

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int ClimbStairs(int n) {
+        if (n <= 2)
+        {
+            return n;
+        }
+
+        int[] cache = new int[n + 1];
+        for (int i = 0; i < cache.Length; i++)
+        {
+            cache[i] = 0;
+        }
+
+        cache[1] = 1;
+        cache[2] = 2;
+
+        for (int i = 3; i < cache.Length; i++)
+        {
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+
+        return cache[n];
+    }
+}
