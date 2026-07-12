58df2aad7a47d9055163337fdad367cf856891c4
Nathaniel Poppe <poppent@mail.uc.edu>
2026-07-12 15:55:30 -0400

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # If we end up after the last index, false
+        # If we land on a 0, false
+
+        curr_pos = 0
+        length = len(nums)
+        last_idx = length - 1
+        while curr_pos < last_idx:
+            if nums[curr_pos] == 0:
+                return False
+
+            curr_pos += nums[curr_pos]
+
+        return True
